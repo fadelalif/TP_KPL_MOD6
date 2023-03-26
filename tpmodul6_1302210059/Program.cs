a7af27fff44154fe269d52f2598f7dcb4e8bc598
fadelalif <fadelkarut@gmail.com>
2023-03-26 16:36:53 +0700

    menambahkan class SayaTubeVideo”

--- tpmodul6_1302210059/Program.cs
+++ tpmodul6_1302210059/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul6_1302210059;
+
+SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design By Contract - Fadel Alif Sadena");
+sayaTubeVideo.IncreasePlayCount(100000001);
+sayaTubeVideo.PrintVideoDetail();
